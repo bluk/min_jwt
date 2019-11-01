2cfcf424f10797bf48a266319aa0f5b165e93bab
Bryant Luk <bryant.luk@bryantluk.com>
2019-11-01 00:55:48 -0500

    Use opaque structs directly instead of Signer trait

--- src/signer.rs
+++ src/signer.rs
@@ -1,45 +1,43 @@
 use base64;
 use ring::hmac;
-use ring::rand::SystemRandom;
+use ring::rand::SecureRandom;
 use ring::signature::{EcdsaKeyPair, RsaKeyPair};
 use serde::Serialize;
 
 use crate::error::Result;
 use crate::header::Header;
 
-pub trait Signer {
-    fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
-    where
-        H: ?Sized + Header,
-        C: ?Sized + Serialize;
-}
-
-// pub fn with_es256(private_key: &[u8]) -> Result<impl Signer> {
-//     let key_pair = EcdsaKeyPair::from_pkcs8(
-//         &ring::signature::ECDSA_P256_SHA256_FIXED_SIGNING,
-//         private_key,
-//     )?;
-//     Ok(ECDSASigner { key_pair })
-// }
-//
-// pub fn with_es384(private_key: &[u8]) -> Result<impl Signer> {
-//     let key_pair = EcdsaKeyPair::from_pkcs8(
-//         &ring::signature::ECDSA_P384_SHA384_FIXED_SIGNING,
-//         private_key,
-//     )?;
-//     Ok(ECDSASigner { key_pair })
-// }
-
-pub fn with_ecdsa(key_pair: EcdsaKeyPair) -> Result<impl Signer> {
-    Ok(ECDSASigner { key_pair })
-}
-
-struct ECDSASigner {
+pub struct ECDSASigner {
     key_pair: EcdsaKeyPair,
 }
 
-impl Signer for ECDSASigner {
-    fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
+impl ECDSASigner {
+    pub fn new(key_pair: EcdsaKeyPair) -> Self {
+        ECDSASigner { key_pair }
+    }
+
+    // pub fn with_es256(private_key: &[u8]) -> Result<ECDSASigner> {
+    //     let key_pair = EcdsaKeyPair::from_pkcs8(
+    //         &ring::signature::ECDSA_P256_SHA256_FIXED_SIGNING,
+    //         private_key,
+    //     )?;
+    //     Ok(ECDSASigner { key_pair })
+    // }
+    //
+    // pub fn with_es384(private_key: &[u8]) -> Result<ECDSASigner> {
+    //     let key_pair = EcdsaKeyPair::from_pkcs8(
+    //         &ring::signature::ECDSA_P384_SHA384_FIXED_SIGNING,
+    //         private_key,
+    //     )?;
+    //     Ok(ECDSASigner { key_pair })
+    // }
+
+    pub fn encode_and_sign<H, C>(
+        &self,
+        header: &H,
+        claims: &C,
+        secure_random: &dyn SecureRandom,
+    ) -> Result<String>
     where
         H: ?Sized + Header,
         C: ?Sized + Serialize,
@@ -48,40 +46,38 @@ impl Signer for ECDSASigner {
         let encoded_claims = to_encoded_part(claims)?;
         let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
 
-        let signature = self
-            .key_pair
-            .sign(&SystemRandom::new(), data_to_sign.as_bytes())?;
+        let signature = self.key_pair.sign(secure_random, data_to_sign.as_bytes())?;
         let signature = base64::encode_config(&signature, base64::URL_SAFE_NO_PAD);
 
         Ok([data_to_sign, signature].join("."))
     }
 }
 
-// pub fn with_hs256(key: &[u8]) -> Result<impl Signer> {
-//     let key = hmac::Key::new(hmac::HMAC_SHA256, key);
-//     Ok(HMACSigner { key })
-// }
-//
-// pub fn with_hs384(key: &[u8]) -> Result<impl Signer> {
-//     let key = hmac::Key::new(hmac::HMAC_SHA384, key);
-//     Ok(HMACSigner { key })
-// }
-//
-// pub fn with_hs512(key: &[u8]) -> Result<impl Signer> {
-//     let key = hmac::Key::new(hmac::HMAC_SHA512, key);
-//     Ok(HMACSigner { key })
-// }
-
-pub fn with_hmac(key: hmac::Key) -> Result<impl Signer> {
-    Ok(HMACSigner { key })
-}
-
-struct HMACSigner {
+pub struct HMACSigner {
     key: hmac::Key,
 }
 
-impl Signer for HMACSigner {
-    fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
+impl HMACSigner {
+    pub fn new(key: hmac::Key) -> Self {
+        HMACSigner { key }
+    }
+
+    // pub fn with_hs256(key: &[u8]) -> Result<HMACSigner> {
+    //     let key = hmac::Key::new(hmac::HMAC_SHA256, key);
+    //     Ok(HMACSigner { key })
+    // }
+    //
+    // pub fn with_hs384(key: &[u8]) -> Result<HMACSigner> {
+    //     let key = hmac::Key::new(hmac::HMAC_SHA384, key);
+    //     Ok(HMACSigner { key })
+    // }
+    //
+    // pub fn with_hs512(key: &[u8]) -> Result<HMACSigner> {
+    //     let key = hmac::Key::new(hmac::HMAC_SHA512, key);
+    //     Ok(HMACSigner { key })
+    // }
+
+    pub fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
     where
         H: ?Sized + Header,
         C: ?Sized + Serialize,
@@ -97,104 +93,106 @@ impl Signer for HMACSigner {
     }
 }
 
-pub fn with_rsa(
-    alg: &'static dyn ring::signature::RsaEncoding,
-    key_pair: RsaKeyPair,
-) -> Result<impl Signer> {
-    Ok(RSASigner { alg, key_pair })
-}
-
-// pub fn with_rs256_der(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PKCS1_SHA256,
-//         ring::signature::RsaKeyPair::from_der(bytes)?,
-//     )
-// }
-//
-// pub fn with_rs384_der(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PKCS1_SHA384,
-//         ring::signature::RsaKeyPair::from_der(bytes)?,
-//     )
-// }
-//
-// pub fn with_rs512_der(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PKCS1_SHA512,
-//         ring::signature::RsaKeyPair::from_der(bytes)?,
-//     )
-// }
-//
-// pub fn with_ps256_der(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PSS_SHA256,
-//         ring::signature::RsaKeyPair::from_der(bytes)?,
-//     )
-// }
-//
-// pub fn with_ps384_der(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PSS_SHA384,
-//         ring::signature::RsaKeyPair::from_der(bytes)?,
-//     )
-// }
-//
-// pub fn with_ps512_der(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PSS_SHA512,
-//         ring::signature::RsaKeyPair::from_der(bytes)?,
-//     )
-// }
-//
-// pub fn with_rs256_pkcs8(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PKCS1_SHA256,
-//         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
-//     )
-// }
-//
-// pub fn with_rs384_pkcs8(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PKCS1_SHA384,
-//         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
-//     )
-// }
-//
-// pub fn with_rs512_pkcs8(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PKCS1_SHA512,
-//         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
-//     )
-// }
-//
-// pub fn with_ps256_pkcs8(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PSS_SHA256,
-//         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
-//     )
-// }
-//
-// pub fn with_ps384_pkcs8(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PSS_SHA384,
-//         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
-//     )
-// }
-//
-// pub fn with_ps512_pkcs8(bytes: &[u8]) -> Result<impl Signer> {
-//     with_rsa(
-//         &ring::signature::RSA_PSS_SHA512,
-//         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
-//     )
-// }
-
-struct RSASigner {
+pub struct RSASigner {
     alg: &'static dyn ring::signature::RsaEncoding,
     key_pair: RsaKeyPair,
 }
 
-impl Signer for RSASigner {
-    fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
+impl RSASigner {
+    pub fn new(alg: &'static dyn ring::signature::RsaEncoding, key_pair: RsaKeyPair) -> Self {
+        RSASigner { alg, key_pair }
+    }
+
+    // pub fn with_rs256_der(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PKCS1_SHA256,
+    //         ring::signature::RsaKeyPair::from_der(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_rs384_der(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PKCS1_SHA384,
+    //         ring::signature::RsaKeyPair::from_der(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_rs512_der(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PKCS1_SHA512,
+    //         ring::signature::RsaKeyPair::from_der(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_ps256_der(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PSS_SHA256,
+    //         ring::signature::RsaKeyPair::from_der(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_ps384_der(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PSS_SHA384,
+    //         ring::signature::RsaKeyPair::from_der(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_ps512_der(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PSS_SHA512,
+    //         ring::signature::RsaKeyPair::from_der(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_rs256_pkcs8(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PKCS1_SHA256,
+    //         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_rs384_pkcs8(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PKCS1_SHA384,
+    //         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_rs512_pkcs8(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PKCS1_SHA512,
+    //         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_ps256_pkcs8(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PSS_SHA256,
+    //         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_ps384_pkcs8(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PSS_SHA384,
+    //         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
+    //     ))
+    // }
+    //
+    // pub fn with_ps512_pkcs8(bytes: &[u8]) -> Result<RSASigner> {
+    //     Ok(Self::new(
+    //         &ring::signature::RSA_PSS_SHA512,
+    //         ring::signature::RsaKeyPair::from_pkcs8(bytes)?,
+    //     ))
+    // }
+
+    pub fn encode_and_sign<H, C>(
+        &self,
+        header: &H,
+        claims: &C,
+        secure_random: &dyn SecureRandom,
+    ) -> Result<String>
     where
         H: ?Sized + Header,
         C: ?Sized + Serialize,
@@ -206,7 +204,7 @@ impl Signer for RSASigner {
         let mut signature = vec![0; self.key_pair.public_modulus_len()];
         self.key_pair.sign(
             self.alg,
-            &SystemRandom::new(),
+            secure_random,
             data_to_sign.as_bytes(),
             &mut signature,
         )?;
