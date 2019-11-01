ceb9c1355a3ce773397e1ed0489737e7b6807b23
Bryant Luk <bryant.luk@bryantluk.com>
2019-11-01 14:59:33 -0500

    Rename types to follow Rust API guidelines

--- src/signer.rs
+++ src/signer.rs
@@ -7,13 +7,13 @@ use serde::Serialize;
 use crate::error::Result;
 use crate::header::Header;
 
-pub struct ECDSASigner {
+pub struct EcdsaSigner {
     key_pair: EcdsaKeyPair,
 }
 
-impl ECDSASigner {
-    pub fn new(key_pair: EcdsaKeyPair) -> Self {
-        ECDSASigner { key_pair }
+impl EcdsaSigner {
+    pub fn with_key_pair(key_pair: EcdsaKeyPair) -> Self {
+        EcdsaSigner { key_pair }
     }
 
     // pub fn with_es256(private_key: &[u8]) -> Result<ECDSASigner> {
@@ -32,6 +32,7 @@ impl ECDSASigner {
     //     Ok(ECDSASigner { key_pair })
     // }
 
+    #[inline]
     pub fn encode_and_sign<H, C>(
         &self,
         header: &H,
@@ -42,8 +43,30 @@ impl ECDSASigner {
         H: ?Sized + Header,
         C: ?Sized + Serialize,
     {
-        let encoded_header = to_encoded_part(header)?;
-        let encoded_claims = to_encoded_part(claims)?;
+        let encoded_header = serde_json::to_string(header)?;
+        let encoded_claims = serde_json::to_string(claims)?;
+        self.encode_and_sign_json_str(&encoded_header, &encoded_claims, secure_random)
+    }
+
+    #[inline]
+    pub fn encode_and_sign_json_str(
+        &self,
+        header: &str,
+        claims: &str,
+        secure_random: &dyn SecureRandom,
+    ) -> Result<String> {
+        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes(), secure_random)
+    }
+
+    #[inline]
+    pub fn encode_and_sign_json_bytes(
+        &self,
+        header: &[u8],
+        claims: &[u8],
+        secure_random: &dyn SecureRandom,
+    ) -> Result<String> {
+        let encoded_header = base64::encode_config(header, base64::URL_SAFE_NO_PAD);
+        let encoded_claims = base64::encode_config(claims, base64::URL_SAFE_NO_PAD);
         let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
 
         let signature = self.key_pair.sign(secure_random, data_to_sign.as_bytes())?;
@@ -53,13 +76,13 @@ impl ECDSASigner {
     }
 }
 
-pub struct HMACSigner {
+pub struct HmacSigner {
     key: hmac::Key,
 }
 
-impl HMACSigner {
-    pub fn new(key: hmac::Key) -> Self {
-        HMACSigner { key }
+impl HmacSigner {
+    pub fn with_key(key: hmac::Key) -> Self {
+        HmacSigner { key }
     }
 
     // pub fn with_hs256(key: &[u8]) -> Result<HMACSigner> {
@@ -77,13 +100,26 @@ impl HMACSigner {
     //     Ok(HMACSigner { key })
     // }
 
+    #[inline]
     pub fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
     where
         H: ?Sized + Header,
         C: ?Sized + Serialize,
     {
-        let encoded_header = to_encoded_part(header)?;
-        let encoded_claims = to_encoded_part(claims)?;
+        let encoded_header = serde_json::to_string(header)?;
+        let encoded_claims = serde_json::to_string(claims)?;
+        self.encode_and_sign_json_str(&encoded_header, &encoded_claims)
+    }
+
+    #[inline]
+    pub fn encode_and_sign_json_str(&self, header: &str, claims: &str) -> Result<String> {
+        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes())
+    }
+
+    #[inline]
+    pub fn encode_and_sign_json_bytes(&self, header: &[u8], claims: &[u8]) -> Result<String> {
+        let encoded_header = base64::encode_config(header, base64::URL_SAFE_NO_PAD);
+        let encoded_claims = base64::encode_config(claims, base64::URL_SAFE_NO_PAD);
         let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
 
         let signature = hmac::sign(&self.key, data_to_sign.as_bytes());
@@ -93,14 +129,14 @@ impl HMACSigner {
     }
 }
 
-pub struct RSASigner {
+pub struct RsaSigner {
     alg: &'static dyn ring::signature::RsaEncoding,
     key_pair: RsaKeyPair,
 }
 
-impl RSASigner {
+impl RsaSigner {
     pub fn new(alg: &'static dyn ring::signature::RsaEncoding, key_pair: RsaKeyPair) -> Self {
-        RSASigner { alg, key_pair }
+        RsaSigner { alg, key_pair }
     }
 
     // pub fn with_rs256_der(bytes: &[u8]) -> Result<RSASigner> {
@@ -187,6 +223,7 @@ impl RSASigner {
     //     ))
     // }
 
+    #[inline]
     pub fn encode_and_sign<H, C>(
         &self,
         header: &H,
@@ -197,8 +234,30 @@ impl RSASigner {
         H: ?Sized + Header,
         C: ?Sized + Serialize,
     {
-        let encoded_header = to_encoded_part(header)?;
-        let encoded_claims = to_encoded_part(claims)?;
+        let encoded_header = serde_json::to_string(header)?;
+        let encoded_claims = serde_json::to_string(claims)?;
+        self.encode_and_sign_json_str(&encoded_header, &encoded_claims, secure_random)
+    }
+
+    #[inline]
+    pub fn encode_and_sign_json_str(
+        &self,
+        header: &str,
+        claims: &str,
+        secure_random: &dyn SecureRandom,
+    ) -> Result<String> {
+        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes(), secure_random)
+    }
+
+    #[inline]
+    pub fn encode_and_sign_json_bytes(
+        &self,
+        header: &[u8],
+        claims: &[u8],
+        secure_random: &dyn SecureRandom,
+    ) -> Result<String> {
+        let encoded_header = base64::encode_config(header, base64::URL_SAFE_NO_PAD);
+        let encoded_claims = base64::encode_config(claims, base64::URL_SAFE_NO_PAD);
         let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
 
         let mut signature = vec![0; self.key_pair.public_modulus_len()];
@@ -214,21 +273,69 @@ impl RSASigner {
     }
 }
 
-fn to_encoded_part<T: Serialize>(v: &T) -> Result<String>
-where
-    T: ?Sized + Serialize,
-{
-    let encoded = serde_json::to_string(v)?;
-    Ok(base64::encode_config(
-        encoded.as_bytes(),
-        base64::URL_SAFE_NO_PAD,
-    ))
-}
-
 #[cfg(test)]
 mod tests {
+    use ring::hmac;
+
+    use super::HmacSigner;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn encode_and_sign_hs256_json_str() {
+        let header_bytes = vec![
+            123, 34, 116, 121, 112, 34, 58, 34, 74, 87, 84, 34, 44, 13, 10, 32, 34, 97, 108, 103,
+            34, 58, 34, 72, 83, 50, 53, 54, 34, 125,
+        ];
+        let header = String::from_utf8(header_bytes).unwrap();
+        let claims_bytes = vec![
+            123, 34, 105, 115, 115, 34, 58, 34, 106, 111, 101, 34, 44, 13, 10, 32, 34, 101, 120,
+            112, 34, 58, 49, 51, 48, 48, 56, 49, 57, 51, 56, 48, 44, 13, 10, 32, 34, 104, 116, 116,
+            112, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109, 47, 105, 115, 95,
+            114, 111, 111, 116, 34, 58, 116, 114, 117, 101, 125,
+        ];
+        let claims = String::from_utf8(claims_bytes).unwrap();
+
+        let hmac_key = String::from("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow");
+        let hmac_key = base64::decode_config(&hmac_key, base64::URL_SAFE_NO_PAD).unwrap();
+
+        let signer = HmacSigner::with_key(hmac::Key::new(hmac::HMAC_SHA256, &hmac_key));
+
+        assert_eq!(
+            signer.encode_and_sign_json_str(&header, &claims).unwrap(),
+            String::from("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.")
+                + &String::from("eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzO")
+                + &String::from("DAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb2")
+                + &String::from("90Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p")
+                + &String::from("1r_wW1gFWFOEjXk")
+        );
+    }
+
+    #[test]
+    fn encode_and_sign_hs256_json_bytes() {
+        let header_bytes = vec![
+            123, 34, 116, 121, 112, 34, 58, 34, 74, 87, 84, 34, 44, 13, 10, 32, 34, 97, 108, 103,
+            34, 58, 34, 72, 83, 50, 53, 54, 34, 125,
+        ];
+        let claims_bytes = vec![
+            123, 34, 105, 115, 115, 34, 58, 34, 106, 111, 101, 34, 44, 13, 10, 32, 34, 101, 120,
+            112, 34, 58, 49, 51, 48, 48, 56, 49, 57, 51, 56, 48, 44, 13, 10, 32, 34, 104, 116, 116,
+            112, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109, 47, 105, 115, 95,
+            114, 111, 111, 116, 34, 58, 116, 114, 117, 101, 125,
+        ];
+
+        let hmac_key = String::from("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow");
+        let hmac_key = base64::decode_config(&hmac_key, base64::URL_SAFE_NO_PAD).unwrap();
+
+        let signer = HmacSigner::with_key(hmac::Key::new(hmac::HMAC_SHA256, &hmac_key));
+
+        assert_eq!(
+            signer
+                .encode_and_sign_json_bytes(&header_bytes, &claims_bytes)
+                .unwrap(),
+            String::from("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.")
+                + &String::from("eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzO")
+                + &String::from("DAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb2")
+                + &String::from("90Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p")
+                + &String::from("1r_wW1gFWFOEjXk")
+        );
     }
 }
