2371b0095f4b6886eccc70d9639e3033b610674b
Bryant Luk <bryant.luk@bryantluk.com>
2019-11-03 09:29:36 -0600

    Use reference to SecureRandom for signers

--- src/signer.rs
+++ src/signer.rs
@@ -6,13 +6,23 @@ use serde::Serialize;
 
 use crate::error::Result;
 
-pub struct EcdsaSigner {
+pub struct EcdsaSigner<'a, T>
+where
+    T: SecureRandom,
+{
     key_pair: EcdsaKeyPair,
+    secure_random: &'a T,
 }
 
-impl EcdsaSigner {
-    pub fn with_key_pair(key_pair: EcdsaKeyPair) -> Self {
-        EcdsaSigner { key_pair }
+impl<'a, T> EcdsaSigner<'a, T>
+where
+    T: SecureRandom,
+{
+    pub fn with_key_pair(key_pair: EcdsaKeyPair, secure_random: &'a T) -> EcdsaSigner<'a, T> {
+        EcdsaSigner {
+            key_pair,
+            secure_random,
+        }
     }
 
     // pub fn with_es256(private_key: &[u8]) -> Result<ECDSASigner> {
@@ -32,43 +42,30 @@ impl EcdsaSigner {
     // }
 
     #[inline]
-    pub fn encode_and_sign<H, C>(
-        &self,
-        header: &H,
-        claims: &C,
-        secure_random: &dyn SecureRandom,
-    ) -> Result<String>
+    pub fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
     where
         H: ?Sized + Serialize,
         C: ?Sized + Serialize,
     {
         let encoded_header = serde_json::to_string(header)?;
         let encoded_claims = serde_json::to_string(claims)?;
-        self.encode_and_sign_json_str(&encoded_header, &encoded_claims, secure_random)
+        self.encode_and_sign_json_str(&encoded_header, &encoded_claims)
     }
 
     #[inline]
-    pub fn encode_and_sign_json_str(
-        &self,
-        header: &str,
-        claims: &str,
-        secure_random: &dyn SecureRandom,
-    ) -> Result<String> {
-        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes(), secure_random)
+    pub fn encode_and_sign_json_str(&self, header: &str, claims: &str) -> Result<String> {
+        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes())
     }
 
     #[inline]
-    pub fn encode_and_sign_json_bytes(
-        &self,
-        header: &[u8],
-        claims: &[u8],
-        secure_random: &dyn SecureRandom,
-    ) -> Result<String> {
+    pub fn encode_and_sign_json_bytes(&self, header: &[u8], claims: &[u8]) -> Result<String> {
         let encoded_header = base64::encode_config(header, base64::URL_SAFE_NO_PAD);
         let encoded_claims = base64::encode_config(claims, base64::URL_SAFE_NO_PAD);
         let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
 
-        let signature = self.key_pair.sign(secure_random, data_to_sign.as_bytes())?;
+        let signature = self
+            .key_pair
+            .sign(self.secure_random, data_to_sign.as_bytes())?;
         let signature = base64::encode_config(&signature, base64::URL_SAFE_NO_PAD);
 
         Ok([data_to_sign, signature].join("."))
@@ -128,14 +125,29 @@ impl HmacSigner {
     }
 }
 
-pub struct RsaSigner {
+pub struct RsaSigner<'a, T>
+where
+    T: SecureRandom,
+{
     alg: &'static dyn ring::signature::RsaEncoding,
     key_pair: RsaKeyPair,
+    secure_random: &'a T,
 }
 
-impl RsaSigner {
-    pub fn new(alg: &'static dyn ring::signature::RsaEncoding, key_pair: RsaKeyPair) -> Self {
-        RsaSigner { alg, key_pair }
+impl<'a, T> RsaSigner<'a, T>
+where
+    T: SecureRandom,
+{
+    pub fn new(
+        alg: &'static dyn ring::signature::RsaEncoding,
+        key_pair: RsaKeyPair,
+        secure_random: &'a T,
+    ) -> RsaSigner<'a, T> {
+        RsaSigner {
+            alg,
+            key_pair,
+            secure_random,
+        }
     }
 
     // pub fn with_rs256_der(bytes: &[u8]) -> Result<RSASigner> {
@@ -223,38 +235,23 @@ impl RsaSigner {
     // }
 
     #[inline]
-    pub fn encode_and_sign<H, C>(
-        &self,
-        header: &H,
-        claims: &C,
-        secure_random: &dyn SecureRandom,
-    ) -> Result<String>
+    pub fn encode_and_sign<H, C>(&self, header: &H, claims: &C) -> Result<String>
     where
         H: ?Sized + Serialize,
         C: ?Sized + Serialize,
     {
         let encoded_header = serde_json::to_string(header)?;
         let encoded_claims = serde_json::to_string(claims)?;
-        self.encode_and_sign_json_str(&encoded_header, &encoded_claims, secure_random)
+        self.encode_and_sign_json_str(&encoded_header, &encoded_claims)
     }
 
     #[inline]
-    pub fn encode_and_sign_json_str(
-        &self,
-        header: &str,
-        claims: &str,
-        secure_random: &dyn SecureRandom,
-    ) -> Result<String> {
-        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes(), secure_random)
+    pub fn encode_and_sign_json_str(&self, header: &str, claims: &str) -> Result<String> {
+        self.encode_and_sign_json_bytes(header.as_bytes(), claims.as_bytes())
     }
 
     #[inline]
-    pub fn encode_and_sign_json_bytes(
-        &self,
-        header: &[u8],
-        claims: &[u8],
-        secure_random: &dyn SecureRandom,
-    ) -> Result<String> {
+    pub fn encode_and_sign_json_bytes(&self, header: &[u8], claims: &[u8]) -> Result<String> {
         let encoded_header = base64::encode_config(header, base64::URL_SAFE_NO_PAD);
         let encoded_claims = base64::encode_config(claims, base64::URL_SAFE_NO_PAD);
         let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
@@ -262,7 +259,7 @@ impl RsaSigner {
         let mut signature = vec![0; self.key_pair.public_modulus_len()];
         self.key_pair.sign(
             self.alg,
-            secure_random,
+            self.secure_random,
             data_to_sign.as_bytes(),
             &mut signature,
         )?;
