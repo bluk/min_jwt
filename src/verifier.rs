41cb07be7052c1899303207db6c1b89f4c036f1b
Bryant Luk <bryant.luk@bryantluk.com>
2019-11-04 15:23:27 -0600

    Update verify signature tests

--- src/verifier.rs
+++ src/verifier.rs
@@ -88,19 +88,31 @@ mod tests {
 
     #[test]
     fn hs256_verify_valid_signature() {
+        let encoded_header = String::from("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9");
+
+        let encoded_claims = String::from(
+            "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb2\
+             90Ijp0cnVlfQ",
+        );
+
         let encoded_signature = String::from("dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk");
 
-        let jwt = String::from("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.")
-            + &String::from("eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzO")
-            + &String::from("DAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb2")
-            + &String::from("90Ijp0cnVlfQ.")
-            + &encoded_signature;
+        let jwt = format!(
+            "{}.{}.{}",
+            encoded_header, encoded_claims, encoded_signature
+        );
 
         let header_bytes = vec![
             123, 34, 116, 121, 112, 34, 58, 34, 74, 87, 84, 34, 44, 13, 10, 32, 34, 97, 108, 103,
             34, 58, 34, 72, 83, 50, 53, 54, 34, 125,
         ];
-        let header = String::from_utf8(header_bytes).unwrap();
+        assert_eq!(
+            base64::encode_config(
+                &String::from_utf8(header_bytes).unwrap(),
+                base64::URL_SAFE_NO_PAD
+            ),
+            encoded_header
+        );
 
         let claims_bytes = vec![
             123, 34, 105, 115, 115, 34, 58, 34, 106, 111, 101, 34, 44, 13, 10, 32, 34, 101, 120,
@@ -108,40 +120,44 @@ mod tests {
             112, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109, 47, 105, 115, 95,
             114, 111, 111, 116, 34, 58, 116, 114, 117, 101, 125,
         ];
-        let claims = String::from_utf8(claims_bytes).unwrap();
+        assert_eq!(
+            base64::encode_config(
+                &String::from_utf8(claims_bytes).unwrap(),
+                base64::URL_SAFE_NO_PAD
+            ),
+            encoded_claims
+        );
 
         let hmac_key = String::from("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow");
         let hmac_key = base64::decode_config(&hmac_key, base64::URL_SAFE_NO_PAD).unwrap();
 
-        let verifier = HmacVerifier::with_key(hmac::Key::new(hmac::HMAC_SHA256, &hmac_key));
+        let hmac_verifier = HmacVerifier::with_key(hmac::Key::new(hmac::HMAC_SHA256, &hmac_key));
 
         let unverified_jwt = UnverifiedJwt::with_str(&jwt).unwrap();
 
-        let signature_verified_jwt = verifier.verify(&unverified_jwt).unwrap();
-
-        let encoded_header = base64::encode_config(&header, base64::URL_SAFE_NO_PAD);
-        let encoded_claims = base64::encode_config(&claims, base64::URL_SAFE_NO_PAD);
-        let data_to_sign = [encoded_header.as_ref(), encoded_claims.as_ref()].join(".");
-
-        assert_eq!(signature_verified_jwt.signed_data(), &data_to_sign);
+        let signature_verified_jwt = hmac_verifier.verify(&unverified_jwt).unwrap();
 
         assert_eq!(
-            signature_verified_jwt.decode_signature().unwrap(),
-            base64::decode_config(&encoded_signature, base64::URL_SAFE_NO_PAD).unwrap()
+            String::from_utf8(signature_verified_jwt.decode_claims().unwrap()).unwrap(),
+            "{\"iss\":\"joe\",\r\n \"exp\":1300819380,\r\n \"http://example.com/is_root\":true}"
         );
     }
 
     #[test]
     fn hs256_verify_invalid_signature() {
-        let encoded_signature = String::from("dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXg");
+        let encoded_header = String::from("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9");
 
-        let jwt = String::from(
-            "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.\
-             eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzO\
-             DAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb2\
-             90Ijp0cnVlfQ.\
-             ",
-        ) + &encoded_signature;
+        let encoded_claims = String::from(
+            "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb2\
+             90Ijp0cnVlfQ",
+        );
+
+        let invalid_encoded_signature = String::from("dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXg");
+
+        let jwt = format!(
+            "{}.{}.{}",
+            encoded_header, encoded_claims, invalid_encoded_signature
+        );
 
         let hmac_key = String::from("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow");
         let hmac_key = base64::decode_config(&hmac_key, base64::URL_SAFE_NO_PAD).unwrap();
--- tests/rs256.rs
+++ tests/rs256.rs
@@ -8,7 +8,7 @@ use jwt_with_ring::UnverifiedJwt;
 use ring::signature::{self, UnparsedPublicKey};
 
 #[test]
-fn rs256_verification_jwt_io_example() {
+fn rs256_verify_valid_signature_jwt_io_example() {
     let encoded_header = String::from("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9");
 
     let encoded_claims = String::from(
@@ -60,4 +60,39 @@ fn rs256_verification_jwt_io_example() {
     assert_eq!("John Doe", claims.name);
     assert_eq!(true, claims.admin);
     assert_eq!(1516239022, claims.iat);
+}
+
+#[test]
+fn rs256_verify_invalid_signature() {
+    let encoded_header = String::from("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9");
+
+    let encoded_claims = String::from(
+        "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWU\
+         sImlhdCI6MTUxNjIzOTAyMn0",
+    );
+
+    let encoded_signature = String::from(
+        "QOstGetfAytaZS82wHcjoTyoqhMyxXiWdR7Nn7A29DNSl0EiXLdwJ6xC6AfgZ\
+         WF1bOsS_TuYI3OG85AmiExREkrS6tDfTQ2B3WXlrr-wp5AokiRbz3_oB4OxG-\
+         W9KcEEbDRcZc0nH3L7LzYptiy1PtAylQGxHTWZXtGz4ht0bAecBgmpdgXMguE\
+         IcoqPJ1n3pIWk_dUZegpqx0Lka21H6XxUTxiy8OcaarA8zdnPUnV6AmNP3ecF\
+         awIFYdvJB_cm-GvpCSbr8G8y_Mllj8f4x9nBH8pQux89_6gUY618iYv7tuPWB\
+         FfEbLxtF2pZS6YC1aSfLQxeNe8djT9YjpvRZA",
+    );
+
+    let jwt = format!(
+        "{}.{}.{}",
+        encoded_header, encoded_claims, encoded_signature
+    );
+
+    let public_key = include_bytes!("rs256_public_key_rsa.der");
+    let public_key =
+        UnparsedPublicKey::new(&signature::RSA_PKCS1_2048_8192_SHA256, &public_key[..]);
+
+    let public_key_verifier = PublicKeyVerifier::with_public_key(public_key);
+
+    let unverified_jwt = UnverifiedJwt::with_str(&jwt).unwrap();
+
+    let error = public_key_verifier.verify(&unverified_jwt).unwrap_err();
+    assert!(error.is_invalid_signature());
 }
