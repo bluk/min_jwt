bf143902c4479d7fe36c679feadc675053f2f41f
Bryant Luk <bryant.luk@bryantluk.com>
2021-12-12 15:20:07 -0600

    Add private trait for Algorithm

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,4 +1,19 @@
-pub trait Algorithm {}
+//! Algorithms used to sign the JWT.
+//!
+//! The algorithms listed here are not comprehensive. The types are used to
+//! represent various properties of the algorithms.
 
+/// A marker trait for an Algorithm.
+pub trait Algorithm: private::Private {}
+
+#[derive(Debug)]
 pub struct Rs256;
+
+impl private::Private for Rs256 {}
 impl Algorithm for Rs256 {}
+
+mod private {
+    pub trait Private {}
+
+    impl<T> Private for &T where T: Private {}
+}
